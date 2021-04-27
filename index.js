e80d6b473167aebfba67fa4eb04d05972604e733
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-04-27 12:09:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function handleClick(e) {
 
 function greet(timeStr) {
 const hour = parseInt(timeStr, 10);
-if ( hour < 12) return "Good Morning"
-if ( hour > 17) return "Good Evening"
+if ( hour < 12) return "Good Morning";
+if ( hour > 17) return "Good Evening";
 return "Good Afternoon"}
 
 
